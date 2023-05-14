4b0599d5a7a097a886191d70b6cf95a844bbbd4c
Zac Vukovic <zac@cyn1x.com>
2023-05-15 08:02:27 +1000

    Rename server module as it shadows built-in http server module

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-from app import server, site_generator, observer
+from app import web_server, site_generator, observer
 
 if __name__ == '__main__':
     observer.run()
     site_generator.build_site()
-    server.run()
+    web_server.run()
